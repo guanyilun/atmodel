0dfcfd66b00c3820103669c19d87356aaf81c9fd
Caglow <caglow@users.sourceforge.net>
2014-10-10 11:25:29 -0600

    Made signal plot without error again.

--- sigtrans.py
+++ sigtrans.py
@@ -15,16 +15,18 @@ def trans(gui, site_file):
             gui.interp.freq_range.min, gui.interp.freq_range.max), dtype='float')
     trans_raw = numpy.array(site.read_from_col(0,
             gui.interp.freq_range.min, gui.interp.freq_range.max, 'COMBIN TRANS'), dtype='float')
-    
+
     return gui.interp.interpolate(freq_raw.tolist(), trans_raw.tolist())
 
 # Compute Signal
 def signal(gui, aperture, site_file, source_file, spec_res):
-    
+
     source = ExcelReader(source_file)
-    freq_raw = numpy.array(source.read_from_col('Hz',freq_range.min, freq_range.max), dtype='float')
-    intens_raw = numpy.array(source.read_from_col('W M-2 Hz-1', freq_range.min, freq_range.max), dtype='float')
+    freq_raw = numpy.array(source.read_from_col('Hz',
+        gui.interp.freq_range.min, gui.interp.freq_range.max), dtype='float')
+    intens_raw = numpy.array(source.read_from_col('W M-2 Hz-1',
+        gui.interp.freq_range.min, gui.interp.freq_range.max), dtype='float')
     intensity = gui.interp.interpolate(freq_raw.tolist(), intens_raw.tolist())
-    
+
     trans_list = trans(gui, site_file)
-    return cal.TS(gui.interp.freq_list, intensity, trans_list, aperture, spec_res)
+    return cal.TS(numpy.array(gui.interp.freq_list), intensity, trans_list, aperture, spec_res)
