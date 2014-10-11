aaad8c0b2770fb6cd9a0eb48a513ed091e5656c3
Caglow <caglow@users.sourceforge.net>
2014-10-11 17:44:09 -0600

    Allow signal to be read in units of Jansky.

--- sigtrans.py
+++ sigtrans.py
@@ -24,8 +24,14 @@ def signal(gui, aperture, site_file, source_file, spec_res):
     source = ExcelReader(source_file)
     freq_raw = numpy.array(source.read_from_col('Hz',
         gui.interp.freq_range.min, gui.interp.freq_range.max), dtype='float')
-    intens_raw = numpy.array(source.read_from_col('W M-2 Hz-1',
-        gui.interp.freq_range.min, gui.interp.freq_range.max), dtype='float')
+
+    try:
+        intens_raw = numpy.array(source.read_from_col('W M-2 Hz-1',
+            gui.interp.freq_range.min, gui.interp.freq_range.max), dtype='float')
+    except:
+        intens_raw = 1e26 * numpy.array(source.read_from_col('Jy',
+            gui.interp.freq_range.min, gui.interp.freq_range.max), dtype='float')
+
     intensity = gui.interp.interpolate(freq_raw.tolist(), intens_raw.tolist())
 
     trans_list = trans(gui, site_file)
