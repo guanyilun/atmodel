486916fca55f931fbf605e8603a3f65aaadbf83e
Sascha Bottoms <edashiba@gmail.com>
2015-05-12 12:52:09 -0700

    Added 50+ microns

--- data/atmos.py
+++ data/atmos.py
@@ -2,7 +2,7 @@
 
 import xlrd
 from xlsxwriter.workbook import Workbook
-altitudes = [0,2,3,4,5,6,9,12,15,20,25,30,35,40]
+altitudes = [0,2,3,4,5,6,9,12,15,20,30,35,40]
 
 #open spreadsheet
 sites = []
@@ -14,7 +14,11 @@ for alt in altitudes:
 		xlrd.open_workbook("Atmosphere/Microwave-"+str(alt)+\
 			"Km-45deg-2000-50000cm-Radiance.xls").sheet_by_index(0),
 		xlrd.open_workbook("Atmosphere/Microwave-"+str(alt)+\
-			"Km-45deg-2000-50000cm-Transmission.xls").sheet_by_index(0)])
+			"Km-45deg-2000-50000cm-Transmission.xls").sheet_by_index(0),
+		xlrd.open_workbook("Atmosphere/Microwave-"+str(alt)+\
+			"Km-45deg-0-200cm-Radiance.xls").sheet_by_index(0),
+		xlrd.open_workbook("Atmosphere/Microwave-"+str(alt)+\
+			"Km-45deg-0-200cm-Transmission.xls").sheet_by_index(0)])
 			
 #read the sheet
 def read(sheet, row_start, col):
@@ -50,27 +54,38 @@ trans1 = []
 freq2 = []
 rad2 = []
 trans2 = []
+#For >50 microns
+freq3 = []
+rad3 = []
+trans3 = []
 
 for site in sites:
 	f1 = read(site[0],1,0)
 	f2 = read(site[2],1,0)
+	f3 = read(site[4],1,0)
 	for i,f1_i in enumerate(f1):
 		f1[i] = f1_i * 29979245800
 	for i,f2_i in enumerate(f2):
 		f2[i] = f2_i * 29979245800
+	for i,f3_i in enumerate(f3):
+		f3[i] = f3_i * 29979245800
 	freq1.append(f1)
 	freq2.append(f2)
+	freq3.append(f3)
 	rad1.append(read(site[0],1,1))
 	rad2.append(read(site[2],1,1))
+	rad3.append(read(site[4],1,1))
 	trans1.append(read(site[1],1,1))
 	trans2.append(read(site[3],1,1))
+	trans3.append(read(site[5],1,1))
 
 for i,alt in enumerate(altitudes):
 	write("Backgrounds/Atmospheric sites/"+str(alt)+"Km-5-50microns.xlsx",\
 		freq1[i],rad1[i],trans1[i],"Freq (Hz)","TOTAL RAD","COMBIN TRANS")
 	write("Backgrounds/Atmospheric sites/"+str(alt)+"Km-0_2-5microns.xlsx",\
 		freq2[i],rad2[i],trans2[i],"Freq (Hz)","TOTAL RAD","COMBIN TRANS")
-
+	write("Backgrounds/Atmospheric sites/"+str(alt)+"Km-50+microns.xlsx",\
+		freq3[i],rad3[i],trans3[i],"Freq (Hz)","TOTAL RAD","COMBIN TRANS")
 
 
 	
